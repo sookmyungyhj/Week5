41e33a3d7f3b7e538ef1ce5a7bf8a85dd4a3383c
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-10-08 00:45:01 +0900

    4

--- project.c
+++ project.c
@@ -4,20 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int num=0;
-	char c;
+	int i,n;
+	int sum = 0;
 	
-	printf("Input a string : ");
+	printf("Input a number:");
+	scanf("%i",&n);
 	
-	while( (c=getchar()) != '\n')
-	{
-		if (c >= '0' && c <= '9')
-		{
-			num++;
-		}
-	}
-	 
-	printf("the number of digits is %i\n", num);
+	for (i=1;i<=n;i++)
+		sum += i;
+		
+	printf("the sum result is %i\n",sum);
 	
 	return 0;
 }
